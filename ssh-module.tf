0ae44c01634bba2439214a2994e3e1ef20dcda09
AKSHAT TANDON <akshat.tandon@stackguardian.io>
2024-07-31 14:58:03 +0200

    add self

--- ssh-module.tf
+++ ssh-module.tf
@@ -1,3 +1,7 @@
 module "storage" {
   source = "git@github.com:StackGuardian/terraform-aws-ec2-instance.git"
 }
+
+module "self" {
+  source = "git@github.com:arunim2405/template-tf-aws-s3-demo-website.git"
+}
