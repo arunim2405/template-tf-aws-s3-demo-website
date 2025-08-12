92ddf2989b057412557b6f5e256f1f60966e3147
Arunim Chaudhary <arunim.chaudhary@stackguardian.io>
2025-08-12 17:54:41 +0530

    min change

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_cors_configuration" "s3_bucket" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET", "HEAD", "POST"]
+    allowed_methods = ["GET", "HEAD", "POST", "PATCH"]
     allowed_origins = ["*"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
